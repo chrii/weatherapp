ad5aa8d3248717da7734b7841f3587b6252aaa44
Chris <c.pintarich@gmail.com>
2017-01-07 18:57:08 +0100

    Update wetterst.sh

--- wetterapp/wetterst.sh
+++ wetterapp/wetterst.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 weather=`lynx -source www.accuweather.com | egrep 'recentlocation' -i | cut -d\" -sf 4 | head -1`
-dather=`date | cut -f 2-4 && echo $weather` && echo $dather >> ~/wetterapp/wetterberichtst
+dather=`date | cut -d\: -f 2-4 && echo $weather` && echo $dather >> ~/wetterapp/wetterberichtst
